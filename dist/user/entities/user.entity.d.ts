36d9ad6c65a75183b0cc436953ef2912137bc24e
iurii-oleinik <iurii.oleinik@alpeso.io>
2025-09-15 18:12:53 +0300

    feat: remove 'dist' folder

--- dist/user/entities/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class User {
-    id: string;
-    login: string;
-    password: string;
-    version: number;
-    createdAt: Date;
-    updatedAt: Date;
-    constructor(partial: Partial<User>);
-}
